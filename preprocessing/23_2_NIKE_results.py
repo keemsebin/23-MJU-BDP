218a0436db691520839e154bd38b2d8b951206bb
[jina4066] <[jina4066@naver.com]>
2023-12-11 15:58:32 +0900

    feat: 국내 데이터 전처리 기능 추가

--- preprocessing/23_2_NIKE_results.py
+++ preprocessing/23_2_NIKE_results.py
@@ -1,55 +1,73 @@
-import re
+# 국내 전처리
 
-# 파일에서 데이터를 읽어오기
-with open('/content/drive/MyDrive/Colab Notebooks/BDP/23_2_NIKE.txt', 'r') as f:
+import re
+import unicodedata
+
+brand = '나이키'
+
+def remove_emoji(string):
+    return ''.join(c for c in string if unicodedata.category(c) not in ['So', 'Cn'])
+
+# '/' 문자 앞뒤에 공백이 있는 경우 이를 먼저 제거한 후 ','로 치환하는 함수
+def replace_slash(string):
+    return re.sub(r'\s?/\s?', ',', string).strip()
+
+# '.' 문자를 찾아 이를 ','로 치환하는 함수
+def replace_period(string):
+    return string.replace('.', ',')
+
+# '('나 ')'를 찾아 이를 제거하는 함수
+def remove_parentheses(string):
+    return string.replace('(', '').replace(')', '')
+
+# brand를 찾아 이를 제거하는 함수
+def remove_brand(string):
+    return string.replace(brand, '')
+
+with open('/content/drive/MyDrive/BDP/23_2_NIKE.txt', 'r') as f:
     lines = f.readlines()
 
-    total_lines = len(lines)  # 전처리 전의 총 라인 수
-    removed_lines = 0  # 제외한 라인 수
-    results = []
+total_lines = len(lines)
+removed_lines = 0
+results = []
 
-    for line in lines:
-        # 맨 앞과 맨 뒤의 콤마를 기준으로 세 부분으로 나누기
-        parts = line.split(',', 2)
-        # 숫자 사이의 콤마를 무시하기 위해 정규 표현식 사용
-        items = [re.sub(r'(\d),(\d)', r'\1\2', part.strip()) for part in parts]
+for line in lines:
+    parts = line.split(',', 2)
+    items = [re.sub(r'(\d),(\d)', r'\1\2', part.strip()) for part in parts]
 
-        if len(items) < 3:  # 콤마가 2개 미만으로 나뉘는 경우, 해당 라인 제외
-            removed_lines += 1
-            continue
-        first = items[0]
-        middle = items[1]
-        last = items[-1]
+    if len(items) < 3:
+        removed_lines += 1
+        continue
+    first = items[0]
+    middle = items[1]
+    last = items[-1]
 
-        # 가격 부분에 숫자가 없는 경우 제외
-        if not re.search(r'\d', last):
-            removed_lines += 1
-            continue
+    if not re.search(r'\d', last):
+        removed_lines += 1
+        continue
 
-        # 가격 부분에서 숫자 이외의 모든 문자 제거
-        last = re.sub(r'\D', '', last)
+    last = re.sub(r'\D', '', last)
 
-        # 가격 부분에서 숫자가 8자리 이상인 경우 제외
-        if len(last) >= 8:
-            removed_lines += 1
-            continue
+    if len(last) >= 8:
+        removed_lines += 1
+        continue
 
-        # 첫번째, 가운데, 마지막 항목 중 하나라도 빈 문자열인 경우 제외
-        # 공백 외의 다른 문자가 있는지 확인
-        if first and middle and last and first.isprintable() and middle.isprintable() and last.isprintable():
-            results.append(', '.join([first, middle, last]))
-        else:
-            removed_lines += 1
+    first = remove_emoji(first).lower()  # 문자열에서 영어를 찾아 모두 소문자로 바꾸는 코드 추가
+    middle = remove_brand(remove_parentheses(replace_period(replace_slash(remove_emoji(middle))))).lower()  # '나이키'를 찾아 이를 제거하는 코드 추가
+    last = remove_emoji(last)
 
-    # 결과 출력
-    print("전처리 전의 총 라인 수: ", total_lines)
-    print("제외한 라인 수: ", removed_lines)
-    print("출력된 라인 수: ", len(results))
+    if first and middle and last and first.isprintable() and middle.isprintable() and last.isprintable():
+        results.append(', '.join([first, middle, last]))
+    else:
+        removed_lines += 12
 
+print("전처리 전의 총 라인 수: ", total_lines)
+print("제외한 라인 수: ", removed_lines)
+print("출력된 라인 수: ", len(results))
+
+for result in results:
+    print(result)
+
+with open('/content/drive/MyDrive/BDP/23_2_NIKE_new.txt', 'w') as f:
     for result in results:
-        print(result)
-
- # 결과를 파일에 저장
-with open('/content/drive/MyDrive/Colab Notebooks/BDP/23_2_NIKE_results.txt', 'w') as f:
-        for result in results:
-            f.write(result + '\n')
+        f.write(result + '\n')
